961d0a511e0823d1398d67cd6575e8c1580efaab
Chelsy New <chelsynew72@gmail.com>
2025-09-08 14:28:56 +0100

    Initial commit of all services

---
+++ shapes-service/src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.TCP,
+        options: { host: '127.0.0.1', port: 3001 },
+      },
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.TCP,
+        options: { host: '127.0.0.1', port: 3002 },
+      },
+      {
+        name: 'CANVAS_SERVICE',
+        transport: Transport.TCP,
+        options: { host: '127.0.0.1', port: 3003 },
+      },
+      {
+        name: 'CHAT_SERVICE',
+        transport: Transport.TCP,
+        options: { host: '127.0.0.1', port: 3005 },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
