6c8139645c2d2babd955345456c334f4db9025b7
nkwadanora <nkwadanora@gmail.com>
2025-09-11 13:35:36 +0100

    import the collaboration service in the collabo module file

---
+++ canvas-service/src/collaboration/collaboration.module.ts
@@ -0,0 +1,10 @@
+
+import { Module } from '@nestjs/common';
+import { CollaborationGateway } from './collaboration.gateway';
+import { CollaborationService } from './collaboration.service';
+
+@Module({
+  providers: [CollaborationGateway, CollaborationService],
+  exports: [CollaborationService],
+})
+export class CollaborationModule {}
