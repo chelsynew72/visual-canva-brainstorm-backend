e856b9c89228834e19c3d8a16f788e9deff044b7
Davyken <davykennang552@gmail.com>
2025-09-12 11:21:26 +0100

    user service ajusted and functional

--- user-service/src/user.module.ts
+++ user-service/src/user.module.ts
@@ -1,5 +1,5 @@
-// libs/user/src/user.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
@@ -7,10 +7,18 @@ import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [UserService],
   controllers: [UserController],
-  exports: [UserService], // export so Auth service can use UserService
+  exports: [UserService], 
 })
 export class UserModule {}
